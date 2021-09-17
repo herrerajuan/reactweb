3f4dd11e2de16ee412f6b89edd835a867d290d29
herrerajuan <jaherreraconde@gmail.com>
2021-09-17 16:33:40 +0200

    folder route

--- react-web/src/components/Cards.jsx
+++ react-web/src/components/Cards.jsx
@@ -10,7 +10,7 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src= '/Users/jaherreraconde/Documents/pruebas/reactweb/react-web/src/images/img-9.jpg'
+              src= '../images/img-9.jpg'
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
